de3c3e0922a904c4babbcc980e53dac5d293f683
Miguel Mercedes <lmyguel0324@gmail.com>
2023-10-19 22:54:05 -0400

    page of guitars completed. Only missing the dinamic route

--- components/guitarra.tsx
+++ components/guitarra.tsx
@@ -1,9 +1,27 @@
+import Image from 'next/future/image';
 import { DatumAttributes } from '../interfaces/guitarras.interface';
+import Link from 'next/link';
+import styles from '../styles/guitarras.module.css'
 
 export default function Guitarra({guitarra}: {guitarra: DatumAttributes}) {
-  console.log(guitarra);
+  
+  const { nombre, precio, descripcion, url } = guitarra;
+  const imagen = guitarra.imagen.data.attributes.formats.medium.url;
 
   return (
-    <div>{guitarra.nombre}</div>
+    <div className={styles.guitarra}>
+      <Image src={imagen} alt={`Imagen de guitarra${nombre}`} width={600} height={400}/>
+
+      <div className={styles.contenido}>
+        <h3>{nombre}</h3>
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>${precio}</p>
+        <Link href={`/guitarras/${url}`}>
+          <a className={styles.enlace}>
+            Ver Producto
+          </a>
+        </Link>
+      </div>
+    </div>
   )
 }
--- pages/tienda.tsx
+++ pages/tienda.tsx
@@ -1,6 +1,7 @@
 import Guitarra from "../components/guitarra";
 import Layout from "../components/layout";
-import { Guitarras } from "../interfaces/guitarras.interface";
+import { Datum, Guitarras } from "../interfaces/guitarras.interface";
+import styles from "../styles/grid.module.css"
 
 // export async function getStaticProps() {
 //   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
@@ -15,37 +16,35 @@ import { Guitarras } from "../interfaces/guitarras.interface";
 
 export async function getServerSideProps() {
   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
-  const data: Guitarras = await respuesta.json();
+  const { data: guitarras }: Guitarras = await respuesta.json();
 
   return {
     props: {
-      data
+      guitarras
     }
   }
 }
 
-export default function Tienda({data}: Guitarras) {
-
-  console.log(data)
+export default function Tienda( {guitarras}: {guitarras: Datum[]}) {
 
   return (
-    <>
-      <Layout 
-        title={'Tienda'} 
-        description={'Tienda, venta de guitarras, instrumentos, GuitarLA.'}
-      >
-        <main className="contenedor">
-          <h1 className="heading">Nuestra Colección</h1>
+    <Layout 
+      title={'Tienda'} 
+      description={'Tienda, venta de guitarras, instrumentos, GuitarLA.'}
+    >
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colección</h1>
 
-          {data.map( guitarra => (
+        <div className={styles.grid}>
+          {guitarras?.map( guitarra => (
             <Guitarra 
               key={guitarra.id}
               guitarra={guitarra.attributes}
             />
           ))}
-        </main>
+        </div>
+      </main>
 
-      </Layout>
-    </>
+    </Layout>
   )
 }
